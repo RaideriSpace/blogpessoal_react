30e2eea68b1ab26a3d5606ad2458fad32cfb0c60
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2025-08-29 17:04:43 -0300

    feat: adiciona rotas

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,26 +1,30 @@
-import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
+import { GithubLogoIcon, LinkedinLogoIcon, WhatsappLogoIcon } from '@phosphor-icons/react'
 
 function Footer() {
 
     let data = new Date().getFullYear()
 
     return (
-        <>
-            <div className="flex justify-center bg-indigo-900 text-white">
-                <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>
-                            Blog Pessoal Generation | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse nossas redes sociais</p>
-                    <div className='flex gap-2'>
-                        <LinkedinLogoIcon size={48} weight='bold' />
-                        <InstagramLogoIcon size={48} weight='bold' />
-                        <FacebookLogoIcon size={48} weight='bold' />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+			<>
+				<div className="flex justify-center bg-indigo-900 text-white">
+					<div className="container flex flex-col items-center py-4">
+						<p className="text-xl font-bold">Blog Pessoal Generation | Copyright: {data}</p>
+						<p className="text-lg">Acesse nossas redes sociais</p>
+						<div className="flex gap-2">
+							<a href="https://www.linkedin.com/in/lucasalvespinheiro" target="_blank">
+								<LinkedinLogoIcon size={48} weight="duotone" />
+							</a>
+							<a href="https://github.com/RaideriSpace" target="_blank">
+								<GithubLogoIcon size={48} weight="duotone" />
+							</a>
+							<a href="https://wa.me/5511989157255" target="_blank">
+								<WhatsappLogoIcon size={48} weight="duotone" />
+							</a>
+						</div>
+					</div>
+				</div>
+			</>
+		);
 }
 
 export default Footer
