983134d02da28cc760bb532dd1d2ad0f1826edad
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2025-09-01 19:20:37 -0300

    feat: adiciona navbar condicional

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,13 @@
-import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useContext, useMemo } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
 	const navigate = useNavigate();
+	const location = useLocation();
+
+	const isLoginPage = useMemo(() => location.pathname == "/login", [location.pathname]);
+	const isCadastroPage = useMemo(() => location.pathname == "/cadastro", [location.pathname]);
 
 	const { handleLogout } = useContext(AuthContext);
 
@@ -21,10 +25,32 @@ function Navbar() {
 				<div className="container flex justify-between text-lg">
 					<Link to={"/home"}>Blog Pessoal </Link>
 					<div className="flex gap-4">
-						Postagens Temas Cadastrar tema Perfil
-						<Link to="/login" onClick={logout} className="hover:underline">
-							Sair
-						</Link>
+						{isLoginPage || isCadastroPage ? (
+							<>
+								{isLoginPage ? (
+									<>
+										<span className="font-bold"> Logar </span>
+										<Link to={"/cadastro"} className="hover:underline">
+											<span> Cadastrar </span>
+										</Link>
+									</>
+								) : (
+									<>
+										<Link to={"/login"} className="hover:underline">
+											<span> Logar </span>
+										</Link>
+										<span className="font-bold"> Cadastrar </span>
+									</>
+								)}
+							</>
+						) : (
+							<div>
+								Postagens Temas Cadastrar tema Perfil &nbsp;
+								<Link to={"/login"} onClick={logout} className="hover:underline">
+									Sair
+								</Link>
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
